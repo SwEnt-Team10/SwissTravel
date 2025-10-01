d59bcab2d2be405d2a01ca2b631bf4c3265276ee
Justin Favey <ju.favey@gmail.com>
2025-10-01 17:46:47 +0200

    chore(firebase): update applicationId for Firebase integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.android.sample"
+        applicationId = "com.github.swent.swisstravel"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
