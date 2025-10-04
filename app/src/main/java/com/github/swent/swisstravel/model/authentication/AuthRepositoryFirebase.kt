d428cc59e637535e3177952d61b649030dfcb188
Adrien Renggli <adrien.renggli@gmail.com>
2025-10-04 15:14:55 +0200

    feat(authentication): add authentication functionalities.
    - Sign in screen.
    - Sign in with google.
    - Log out function.
    - Sign in with google composable.
    - Web client id.

---
+++ app/src/main/java/com/github/swent/swisstravel/model/authentication/AuthRepositoryFirebase.kt
@@ -0,0 +1,66 @@
+package com.github.swent.swisstravel.model.authentication
+
+/** This file is largely adapted from the bootcamp solution. */
+
+import androidx.credentials.Credential
+import androidx.credentials.CustomCredential
+import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.tasks.await
+
+/**
+ * A Firebase implementation of [AuthRepository].
+ *
+ * Retrieves a Google ID token via Credential Manager and authenticates the user with Firebase. Also
+ * handles sign-out and credential state clearing.
+ *
+ * @param auth The [FirebaseAuth] instance for Firebase authentication.
+ * @param helper A [GoogleSignInHelper] to extract Google ID token credentials and convert them to
+ *   Firebase credentials.
+ */
+class AuthRepositoryFirebase(
+    private val auth: FirebaseAuth = Firebase.auth,
+    private val helper: GoogleSignInHelper = DefaultGoogleSignInHelper()
+) : AuthRepository {
+
+    fun getGoogleSignInOption(serverClientId: String) =
+        GetSignInWithGoogleOption.Builder(serverClientId = serverClientId).build()
+
+    override suspend fun signInWithGoogle(credential: Credential): Result<FirebaseUser> {
+        return try {
+            if (credential is CustomCredential && credential.type == TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                val idToken = helper.extractIdTokenCredential(credential.data).idToken
+                val firebaseCred = helper.toFirebaseCredential(idToken)
+
+                // Sign in with Firebase
+                val user =
+                    auth.signInWithCredential(firebaseCred).await().user
+                        ?: return Result.failure(
+                            IllegalStateException("Login failed: Could not retrieve user information")) // todo use string resource
+                return Result.success(user)
+            } else {
+                return Result.failure(
+                    IllegalStateException("Login failed: Credential is not of type Google ID")) // todo use string resource
+            }
+        } catch (e: Exception) {
+            Result.failure(
+                IllegalStateException("Login failed: ${e.localizedMessage ?: "Unexpected error"}")) // todo use string resource
+        }
+    }
+
+    override fun signOut(): Result<Unit> {
+        return try {
+            // Firebase sign out
+            auth.signOut()
+
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(
+                IllegalStateException("Logout failed: ${e.localizedMessage ?: "Unexpected error."}")) // todo use string resource
+        }
+    }
+}
---
+++ app/src/main/java/com/github/swent/swisstravel/ui/authentication/SignInScreen.kt
@@ -0,0 +1,114 @@
+package com.github.swent.swisstravel.ui.authentication
+
+/** This file is largely adapted from the bootcamp solution. */
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.credentials.CredentialManager
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.swent.swisstravel.R
+import com.github.swent.swisstravel.ui.composable.GoogleSignInButton
+
+object SignInScreenTestTags {
+    const val APP_LOGO = "appLogo"
+    const val LOGIN_TITLE = "loginTitle"
+    const val LOGIN_BUTTON = "loginButton"
+}
+
+@Composable
+fun SignInScreen(
+    authViewModel: SignInViewModel = viewModel(),
+    credentialManager: CredentialManager = CredentialManager.create(LocalContext.current),
+    onSignedIn: () -> Unit = {}
+) {
+    val context = LocalContext.current
+    val uiState by authViewModel.uiState.collectAsState()
+
+    // Show error message if login fails
+    LaunchedEffect(uiState.errorMsg) {
+        uiState.errorMsg?.let {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            authViewModel.clearErrorMsg()
+        }
+    }
+
+    // Navigate to overview screen on successful login
+    LaunchedEffect(uiState.user) {
+        uiState.user?.let {
+            Toast.makeText(context, "Login successful!", Toast.LENGTH_SHORT).show() // todo use string resource
+            onSignedIn()
+        }
+    }
+
+    // The main container for the screen
+    // A surface container using the 'background' color from the theme
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        content = { padding ->
+            Column(
+                modifier = Modifier.fillMaxSize().padding(padding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                // App Logo Image
+                Image(
+                    painter = painterResource(id = R.drawable.swisstravel_logo),
+                    contentDescription = "App Logo",
+                    modifier = Modifier.size(250.dp).testTag(SignInScreenTestTags.APP_LOGO)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Welcome Text
+                Text(
+                    modifier = Modifier.testTag(SignInScreenTestTags.LOGIN_TITLE),
+                    text = "Welcome to SwissTravel", // todo use string resource
+                    style = MaterialTheme
+                        .typography.headlineLarge.copy(fontSize = 57.sp, lineHeight = 64.sp),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.height(48.dp))
+
+                // Authenticate With Google Button
+                if (uiState.isLoading) {
+                    CircularProgressIndicator(modifier = Modifier.size(48.dp))
+                } else {
+                    GoogleSignInButton(onSignInClick = { authViewModel.signIn(context, credentialManager) })
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun SignInScreenPreview() {
+    SignInScreen()
+}
