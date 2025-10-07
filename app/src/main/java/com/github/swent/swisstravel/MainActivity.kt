df227727199f95fe99ac4da361e1ab7f8743428d
elliott-git <elliottrenaud@gmail.com>
2025-10-07 17:48:57 +0200

    feat: add MapBox Map api to the project Added the MapBox Map api to the project by setting up dependencies and the settings for gradle. Map can be displayed and is working for the composable example on the website (get started)
    # Conflicts:
    #	app/src/main/java/com/github/swent/swisstravel/MainActivity.kt
    
    # Conflicts:
    #	app/build.gradle.kts
    #	settings.gradle.kts

--- app/src/main/java/com/github/swent/swisstravel/MainActivity.kt
+++ app/src/main/java/com/github/swent/swisstravel/MainActivity.kt
@@ -24,14 +24,7 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      SampleAppTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
-            color = MaterialTheme.colorScheme.background) {
-              Greeting("Android")
-            }
-      }
+        MapLocationScreen()
     }
   }
 }
