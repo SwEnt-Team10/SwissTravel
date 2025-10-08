c2e7f9bfc45e961066e67e84fe6b9fc758e273a8
Adrien Renggli <adrien.renggli@gmail.com>
2025-10-08 14:09:39 +0200

    test(authentication): add tests for GoogleSignIn.

---
+++ app/src/test/java/com/github/swent/swisstravel/model/authentication/GoogleSignInHelperTest.kt
@@ -0,0 +1,36 @@
+package com.github.swent.swisstravel.model.authentication
+
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.verify
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class DefaultGoogleSignInHelperTest {
+
+    private lateinit var googleSignInHelper: DefaultGoogleSignInHelper
+    private val mockAuthCredential: AuthCredential = mockk()
+
+    @Before
+    fun setUp() {
+        googleSignInHelper = DefaultGoogleSignInHelper()
+        mockkStatic(GoogleIdTokenCredential::class)
+        mockkStatic(GoogleAuthProvider::class)
+    }
+
+    @Test
+    fun toFirebaseCredential_callsCorrectMethod() {
+        val idToken = "test_id_token"
+        every { GoogleAuthProvider.getCredential(idToken, null) } returns mockAuthCredential
+
+        val result = googleSignInHelper.toFirebaseCredential(idToken)
+
+        assertEquals(mockAuthCredential, result)
+        verify { GoogleAuthProvider.getCredential(idToken, null) }
+    }
+}
