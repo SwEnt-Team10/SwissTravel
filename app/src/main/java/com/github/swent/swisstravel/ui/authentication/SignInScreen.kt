f486279a20e513cdd822af22224d3118aadfabde
Adrien Renggli <adrien.renggli@gmail.com>
2025-10-08 15:44:21 +0200

    test(SignInScreen): remove preview.

--- app/src/main/java/com/github/swent/swisstravel/ui/authentication/SignInScreen.kt
+++ app/src/main/java/com/github/swent/swisstravel/ui/authentication/SignInScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.credentials.CredentialManager
@@ -102,10 +101,4 @@ fun SignInScreen(
           }
         }
       })
-}
-
-@Preview
-@Composable
-fun SignInScreenPreview() {
-  SignInScreen()
 }
