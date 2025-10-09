420aeddfc28df6fa483b24b44616402569ea98bd
Arnaud Favre <arnaud.favre@epfl.ch>
2025-10-09 13:55:56 +0200

    chore(tests): remove duplicate test annotation
    Removes a redundant `@Test` annotation and a blank line from `NavigationTest.kt`.

--- app/src/androidTest/java/com/android/swisstravel/ui/navigation/NavigationTest.kt
+++ app/src/androidTest/java/com/android/swisstravel/ui/navigation/NavigationTest.kt
@@ -62,8 +62,7 @@ class NavigationTest : SwissTravelTest() {
         .performClick()
     composeTestRule.onNodeWithTag(NavigationTestTags.PROFILE_TAB).assertIsDisplayed().performClick()
   }
-
-  @Test
+  
   @Test
   fun navigationBetweenTabsWorks() {
     //        composeTestRule.onNodeWithTag(NavigationTestTags.CURRENT_TRIP_TAB).performClick()
