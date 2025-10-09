a83489b6183487f47382d0b0341aa4fe528f0ec8
Arnaud Favre <arnaud.favre@epfl.ch>
2025-10-09 13:36:09 +0200

    chore(tests): add comment to clarify test tags
    Adds a comment `/* Test tags */` to the `NavigationTestTags` object to clarify the purpose of the constants defined within the file.

--- app/src/main/java/com/github/swent/swisstravel/ui/navigation/NavigationTestTags.kt
+++ app/src/main/java/com/github/swent/swisstravel/ui/navigation/NavigationTestTags.kt
@@ -2,6 +2,7 @@ package com.github.swent.swisstravel.ui.navigation
 
 /** Heavily inspired from the B3 of the SwEnt course at EPFL */
 object NavigationTestTags {
+  /* Test tags */
   const val BOTTOM_NAVIGATION_MENU = "BottomNavigationMenu"
   const val PROFILE_TAB = "ProfileTab"
   const val MY_TRIPS_TAB = "MyTripsTab"
