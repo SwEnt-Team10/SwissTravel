1ddcd712e0e68e84e87d91955f40aa8e127bc87f
cldylan <dylan.callahan@epfl.ch>
2025-10-10 17:53:57 +0200

    feat(trips): add TripsRepository interface
    Added the interface for a repository to store trips

---
+++ app/src/main/java/com/github/swent/swisstravel/model/trip/TripsRepository.kt
@@ -0,0 +1,39 @@
+package com.github.swent.swisstravel.model.trip
+
+/** Inspired by the SwEnt Bootcamp solution. Represents a repository that manages Trips. */
+interface TripsRepository {
+
+  /** Generates and returns a new unique identifier for a Trip. */
+  fun getNewUid(): String
+
+  /**
+   * Retrieves all Trips from the repository.
+   *
+   * @return A list of all Trips.
+   */
+  suspend fun getAllTrips(): List<Trip>
+
+  /**
+   * Retrieves a specific Trip by its unique identifier.
+   *
+   * @param tripID The unique identifier of the Trip to retrieve.
+   * @return The Trip with the specified identifier.
+   * @throws Exception if the Trip is not found.
+   */
+  suspend fun getTrip(tripID: String): Trip
+
+  /**
+   * Adds a new Trip to the repository.
+   *
+   * @param trip The Trip to add.
+   */
+  suspend fun addTrip(trip: Trip)
+
+  /**
+   * Deletes a Trip from the repository.
+   *
+   * @param tripID The unique identifier of the Trip to delete.
+   * @throws Exception if the Trip is not found.
+   */
+  suspend fun deleteTrip(tripID: String)
+}
