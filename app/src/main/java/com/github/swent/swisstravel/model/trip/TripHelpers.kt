d288bae766b0badc8090f6403dc1a184f97b0ac8
cldylan <dylan.callahan@epfl.ch>
2025-10-13 18:48:15 +0200

    refactor(trip): move trip element retrieval functions to TripHelpers

--- app/src/main/java/com/github/swent/swisstravel/model/trip/TripHelpers.kt
+++ app/src/main/java/com/github/swent/swisstravel/model/trip/TripHelpers.kt
@@ -56,3 +56,44 @@ fun Trip.isPast(): Boolean {
   val now = ZonedDateTime.now()
   return tripProfile.endDate.toZonedDateTime().isBefore(now)
 }
+
+/**
+ * Returns all trip elements ordered by start date.
+ *
+ * @return The list of trip elements.
+ */
+fun Trip.getAllTripElementsOrdered(): List<TripElement> {
+  val elements = mutableListOf<TripElement>()
+  elements.addAll(routeSegments.map { TripElement.TripSegment(it) })
+  elements.addAll(activities.map { TripElement.TripActivity(it) })
+  return elements.sortedBy { it.startDate.seconds }
+}
+
+/**
+ * Returns the upcoming trip elements.
+ *
+ * @param time The current time.
+ * @param getCurrent If true, returns the current trip element too, otherwise only the upcoming
+ * @return The list of upcoming trip elements.
+ */
+fun Trip.getUpcomingTripElements(
+    time: Timestamp = Timestamp.now(),
+    getCurrent: Boolean = false
+): List<TripElement> {
+  val allElements = getAllTripElementsOrdered()
+
+  val firstUpcomingIndex =
+      if (getCurrent) {
+        allElements.indexOfFirst { it.endDate.seconds > time.seconds }
+      } else {
+        allElements.indexOfFirst { it.startDate.seconds >= time.seconds }
+      }
+
+  /* There was no event during and after the current time */
+  return if (firstUpcomingIndex == -1) emptyList()
+  /* Return all upcoming events */
+  else allElements.subList(firstUpcomingIndex, allElements.size)
+}
+
+/** Returns the total time of the trip in hours. */
+fun Trip.getTotalTime(): Double = tripProfile.getTotalTime()
