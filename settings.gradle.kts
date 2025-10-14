53470f6a0017add9340f12205c4eb3176f1e3ce8
Vincent du Fresne <vincent@du-fresne.ch>
2025-10-14 22:26:37 +0200

    chore(ci): add new MAP_BOX_DOWNLOAD_SECRET and configure CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,16 @@ pluginManagement {
             }
         }
         mavenCentral()
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            credentials {
+                username = "mapbox"
+                password = System.getenv("MAPBOX_DOWNLOADS_TOKEN")
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
         gradlePluginPortal()
     }
 }
@@ -18,14 +28,14 @@ dependencyResolutionManagement {
         mavenCentral()
         maven {
             url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
             credentials {
                 // used for private mapbox dependencies such as
                 // "com.mapbox.navigationcore:android-ndk27:3.16.0-beta.1"
                 username = "mapbox"
-                password = System.getenv("MAPBOX_ACCESS_TOKEN")
+                password = System.getenv("MAPBOX_DOWNLOADS_TOKEN")
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
             }
         }
     }
