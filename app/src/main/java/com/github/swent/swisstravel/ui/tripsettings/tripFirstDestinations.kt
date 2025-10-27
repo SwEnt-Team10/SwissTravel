ec952fed7894d2590f2d5383b95c3d18ccf55d46
JstnFv <justin.favey@epfl.ch>
2025-10-27 17:24:18 +0100

    refactor(branch) : refactor branch name

---
+++ app/src/main/java/com/github/swent/swisstravel/ui/tripsettings/tripFirstDestinations.kt
@@ -0,0 +1,10 @@
+package com.github.swent.swisstravel.ui.tripsettings
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun FirstDestinationScreen(
+    viewModel: TripSettingsViewModel = viewModel(),
+    onNext: () -> Unit = {}
+) {}
