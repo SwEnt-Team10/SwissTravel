c002caf472bd9fc32b45091f72bd73da9d96beb0
Dylan Callahan <dylanscallahan@proton.me>
2025-10-28 20:24:43 +0100

    fix(TripSettingsTest) fix import
    - Added missing import due to merge

--- app/src/androidTest/java/com/android/swisstravel/ui/tripSettings/TripSettingsTests.kt
+++ app/src/androidTest/java/com/android/swisstravel/ui/tripSettings/TripSettingsTests.kt
@@ -1,12 +1,13 @@
-package com.android.swisstravel.ui.tripSettings
+package com.android.swisstravel.ui.tripsettings
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import com.android.swisstravel.ui.mytrips.FakeTripsRepository
 import com.android.swisstravel.ui.profile.FakeUserRepository
 import com.android.swisstravel.utils.SwissTravelTest
+import com.github.swent.swisstravel.model.trip.Trip
+import com.github.swent.swisstravel.model.trip.TripsRepository
 import com.github.swent.swisstravel.model.user.Preference
 import com.github.swent.swisstravel.ui.composable.PreferenceSelectorTestTags
 import com.github.swent.swisstravel.ui.composable.ToggleTestTags
