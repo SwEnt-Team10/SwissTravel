1aa4dafa7d561f4ecf428d3b8cac7da0b0783d45
JstnFv <justin.favey@epfl.ch>
2025-10-29 14:30:14 +0100

    format(navigation) : Fix formating issues with NavigationActions.kt

--- app/src/main/java/com/github/swent/swisstravel/ui/navigation/NavigationActions.kt
+++ app/src/main/java/com/github/swent/swisstravel/ui/navigation/NavigationActions.kt
@@ -37,7 +37,9 @@ sealed class Screen(
 
   object TripSettings3 : Screen(route = "trip_settings_3", name = "Trip Settings 3")
 
-  object TripSettingsArrivalDeparture : Screen(route = "arrival_departure", name = "Arrival/Departure")
+  object TripSettingsArrivalDeparture :
+      Screen(route = "arrival_departure", name = "Arrival/Departure")
+
   object EditTrip : Screen(route = "edit_trip/{tripId}", name = "Edit trip") {
     fun createRoute(tripId: String) = "edit_trip/${URLEncoder.encode(tripId, "UTF-8")}"
   }
