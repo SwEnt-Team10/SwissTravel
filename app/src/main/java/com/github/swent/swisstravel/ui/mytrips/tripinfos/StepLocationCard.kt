68df077a41e8dbaa40ea118a8d460856b091c546
elliott-git <elliottrenaud@gmail.com>
2025-11-07 15:06:09 +0100

    fix: remove preview for line coverage Removed the part that preview the StepLocationCard for improving line coverage

--- app/src/main/java/com/github/swent/swisstravel/ui/mytrips/tripinfos/StepLocationCard.kt
+++ app/src/main/java/com/github/swent/swisstravel/ui/mytrips/tripinfos/StepLocationCard.kt
@@ -18,10 +18,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.swent.swisstravel.R
-import com.github.swent.swisstravel.model.trip.Coordinate
 import com.github.swent.swisstravel.model.trip.Location
 
 /** A card displaying the step number and location name for a trip step. */
@@ -66,13 +64,4 @@ fun StepLocationCard(int: Int, location: Location) {
               Spacer(modifier = Modifier.width(24.dp))
             }
       }
-}
-
-@Preview
-@Composable
-fun StepLocationCardPreview() {
-  StepLocationCard(
-      int = 1,
-      location =
-          Location(name = "Zurich Hauptbahnhof", coordinate = Coordinate(47.378177, 8.540192)))
 }
