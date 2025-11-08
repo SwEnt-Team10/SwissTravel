d2842c8f1129b0fddfed1591c6d86702ea5e094a
Adrien Renggli <adrien.renggli@gmail.com>
2025-11-08 14:13:03 +0100

    feat(activites): create a list of activities available to the user.

--- app/src/main/java/com/github/swent/swisstravel/ui/tripcreation/TripCreationViewModel.kt
+++ app/src/main/java/com/github/swent/swisstravel/ui/tripcreation/TripCreationViewModel.kt
@@ -8,10 +8,13 @@ import com.github.swent.swisstravel.model.trip.Trip
 import com.github.swent.swisstravel.model.trip.TripProfile
 import com.github.swent.swisstravel.model.trip.TripsRepository
 import com.github.swent.swisstravel.model.trip.TripsRepositoryFirestore
+import com.github.swent.swisstravel.model.trip.activity.Activity
+import com.github.swent.swisstravel.model.trip.activity.ActivityRepositoryMySwitzerland
 import com.github.swent.swisstravel.model.user.Preference
 import com.github.swent.swisstravel.model.user.UserRepository
 import com.github.swent.swisstravel.model.user.UserRepositoryFirebase
 import com.google.firebase.Timestamp
+import java.lang.Thread.sleep
 import java.time.LocalDate
 import java.time.ZoneId
 import kotlinx.coroutines.channels.Channel
@@ -20,6 +23,9 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+
+// Wait before fetching new data to avoid overwhelming the api
+private const val SLEEP = 1000.toLong()
 
 /** * Data class representing the start and end dates of a trip. */
 data class TripDate(val startDate: LocalDate? = null, val endDate: LocalDate? = null)
@@ -104,6 +110,56 @@ class TripSettingsViewModel(
     }
   }
 
+  /** Select activities near predetermined stops and with user preferences. */
+  private suspend fun addActivities(): List<Activity> {
+    val activityRepository = ActivityRepositoryMySwitzerland()
+    val output = mutableListOf<Activity>()
+
+    // Get activities near stops
+    val allActivitiesNear = mutableListOf<Activity>()
+    for (place in _tripSettings.value.destinations) {
+      val activities = activityRepository.getActivitiesNear(place.coordinate, 15000, 20)
+      allActivitiesNear.addAll(activities)
+      sleep(SLEEP)
+    }
+    output.addAll(allActivitiesNear.distinct())
+
+    // Get activities with preferences
+    val preferences = _tripSettings.value.preferences.toMutableList()
+    if (preferences.isNotEmpty()) {
+      val allActivitiesPref = mutableListOf<Activity>()
+
+      val wheelChair = preferences.contains(Preference.WHEELCHAIR_ACCESSIBLE)
+      val publicTransport = preferences.contains(Preference.PUBLIC_TRANSPORT)
+
+      // Mandatory filters
+      val always = mutableListOf<Preference>()
+
+      if (wheelChair) {
+        preferences.remove(Preference.WHEELCHAIR_ACCESSIBLE)
+        always.add(Preference.WHEELCHAIR_ACCESSIBLE)
+      }
+      if (publicTransport) {
+        preferences.remove(Preference.PUBLIC_TRANSPORT)
+        always.add(Preference.PUBLIC_TRANSPORT)
+      }
+
+      for (preference in preferences) {
+        val activities = activityRepository.getActivitiesByPreferences(always + preference, 100)
+        allActivitiesPref.addAll(activities)
+        sleep(SLEEP)
+      }
+      allActivitiesPref.distinct()
+
+      // Select activities that are near and respects user preferences
+      output.filter { activity -> allActivitiesPref.contains(activity) }
+    }
+
+    setDestinations(_tripSettings.value.destinations + output.map { it.location })
+
+    return output
+  }
+
   /**
    * Saves the current trip settings as a new Trip in the repository.
    *
@@ -112,6 +168,8 @@ class TripSettingsViewModel(
   fun saveTrip() {
     viewModelScope.launch {
       try {
+        val activities = addActivities()
+
         val settings = _tripSettings.value
         val newUid = tripsRepository.getNewUid()
         val user = userRepository.getCurrentUser()
@@ -147,7 +205,7 @@ class TripSettingsViewModel(
                 ownerId = user.uid,
                 locations = settings.destinations,
                 routeSegments = emptyList(),
-                activities = emptyList(),
+                activities = activities,
                 tripProfile = tripProfile,
                 isFavorite = false,
                 isCurrentTrip = false)
