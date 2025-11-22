0e62bbd75fe39b82026ffed91396948e4ff36c57
elliott-git <elliottrenaud@gmail.com>
2025-11-22 20:20:08 +0100

    feat: add TripRepositoryLocal Add a local trip repository (same code as the bootcamp)

---
+++ app/src/main/java/com/github/swent/swisstravel/model/trip/TripRepositoryLocal.kt
@@ -0,0 +1,41 @@
+package com.github.swent.swisstravel.model.trip
+
+class TripRepositoryLocal(
+    private val trips: MutableList<Trip> = mutableListOf(),
+    private var counter: Int = 0
+) : TripsRepository{
+    override fun getNewUid(): String {
+        return (counter++).toString()
+    }
+
+    override suspend fun getAllTrips(): List<Trip> {
+        return trips
+    }
+
+    override suspend fun getTrip(tripId: String): Trip {
+        return trips.find { it.uid == tripId }
+            ?: throw Exception("TripRepositoryLocal: Trip not found")
+    }
+
+    override suspend fun addTrip(trip: Trip) {
+        trips.add(trip)
+    }
+
+    override suspend fun editTrip(tripId: String, updatedTrip: Trip) {
+        val index = trips.indexOfFirst { it.uid == tripId }
+        if (index != -1) {
+            trips[index] = updatedTrip
+        } else {
+            throw Exception("TripRepositoryLocal: Trip not found")
+        }
+    }
+
+    override suspend fun deleteTrip(tripId: String) {
+        val index = trips.indexOfFirst { it.uid == tripId }
+        if (index != -1) {
+            trips.removeAt(index)
+        } else {
+            throw Exception("TripRepositoryLocal: Trip not found")
+        }
+    }
+}
