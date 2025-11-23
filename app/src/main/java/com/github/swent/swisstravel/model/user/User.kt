edf6dfdf9481d12b3352d6efc47dfd8842460335
Lionel Hegetschweiler <lionel.hegetschweiler@epfl.ch>
2025-11-23 19:15:32 +0100

    feat(user): expand user data model
    This commit expands the `User` data model to include more detailed information and introduces related data classes for statistics and social features.
    
    - A `biography` field has been added to the `User` data class.
    - The `User` model now includes a list of `Friend` objects.
    - A new `UserStats` data class has been added to track user statistics, such as total trips, travel time, and distance. The `User` model now contains an instance of `UserStats`.
    - The `Friend` data class and `FriendStatus` enum have been introduced to manage friend relationships and their states (e.g., PENDING, ACCEPTED).

--- app/src/main/java/com/github/swent/swisstravel/model/user/User.kt
+++ app/src/main/java/com/github/swent/swisstravel/model/user/User.kt
@@ -1,18 +1,64 @@
 package com.github.swent.swisstravel.model.user
 
+import com.github.swent.swisstravel.model.trip.TransportMode
+
 /**
  * User data model representing a user in the application.
  *
  * @property uid Unique identifier for the user.
  * @property name Full name of the user.
+ * @property biography Short biography of the user.
  * @property email Email address of the user.
  * @property profilePicUrl URL to the user's profile picture.
  * @property preferences List of user preferences.
+ * @property friends List of user's friends.
+ * @property stats User statistics.
  */
 data class User(
     val uid: String,
     val name: String,
+    val biography: String,
     val email: String,
     val profilePicUrl: String,
-    val preferences: List<Preference>
+    val preferences: List<Preference>,
+    val friends: List<Friend>,
+    val stats: UserStats = UserStats()
 )
+
+/**
+ * User data model representing a user's various statistics
+ *
+ * @property totalTrips Total number of trips the user has taken
+ * @property totalTravelMinutes Total travel hours the user has taken
+ * @property totalDistanceKm Total distance the user has traveled
+ * @property uniqueLocations Number of unique locations the user has traveled to
+ * @property mostUsedTransportMode Most used transport mode by the user
+ * @property longestRouteSegmentKm Longest route segment the user has traveled
+ */
+data class UserStats(
+    val totalTrips: Int = 0,
+    val totalTravelMinutes: Int = 0,
+    val totalDistanceKm: Double = 0.0,
+    val uniqueLocations: Int = 0,
+    val mostUsedTransportMode: TransportMode? = null,
+    val longestRouteSegmentKm: Double = 0.0
+)
+
+/**
+ * A data model representing a friend
+ *
+ * @property uid Unique identifier for the friend
+ * @property status Status of the friend request
+ */
+data class Friend(val uid: String, val status: FriendStatus)
+
+/**
+ * Enum representing the status of a friend request
+ *
+ * @property PENDING Pending friend request
+ * @property ACCEPTED Accepted friend request
+ */
+enum class FriendStatus {
+  PENDING,
+  ACCEPTED
+}
