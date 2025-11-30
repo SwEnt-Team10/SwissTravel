200cba7572770835081e0ad98201f774f86aa4ba
elliott-git <elliottrenaud@gmail.com>
2025-11-30 14:10:44 +0100

    feat: add test for viewModel Added test for AddphotosViewModel

---
+++ app/src/test/java/com/github/swent/swisstravel/ui/trip/addphotos/AddPhotosViewModelTest.kt
@@ -0,0 +1,123 @@
+package com.github.swent.swisstravel.ui.trip.addphotos
+
+import androidx.core.net.toUri
+import com.github.swent.swisstravel.model.trip.Trip
+import com.github.swent.swisstravel.model.trip.TripProfile
+import com.github.swent.swisstravel.model.trip.TripRepositoryLocal
+import com.github.swent.swisstravel.model.trip.TripsRepository
+import com.github.swent.swisstravel.ui.trip.tripinfos.addphotos.AddPhotosViewModel
+import com.google.firebase.Timestamp
+import kotlin.test.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+/** Unit tests for AddPhotosViewModel */
+class AddPhotosViewModelTest {
+    // Fake values
+    private val fakeUris = listOf("uri1".toUri(), "uri2".toUri())
+    // Fake trip
+    private val fakeTrip =
+        Trip(
+            uid = "1",
+            name = "fake trip",
+            ownerId = "100",
+            locations = emptyList(),
+            routeSegments = emptyList(),
+            activities = emptyList(),
+            tripProfile = TripProfile(startDate = Timestamp.now(), endDate = Timestamp.now()),
+            isFavorite = true,
+            isCurrentTrip = true,
+            listUri = fakeUris)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `test loadPhotos correctly load the photos`() = runTest {
+        try {
+            // Set correctly the dispatcher
+            val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+            Dispatchers.setMain(testDispatcher)
+
+            // Set the model
+            val tripRepository = mock(TripsRepository::class.java)
+            val addPhotosViewModel = AddPhotosViewModel(tripRepository)
+
+            // Set up the mock to return the fake trip
+            `when`(tripRepository.getTrip("0")).thenReturn(fakeTrip)
+
+            // Load photos and verify that it is correctly done
+            addPhotosViewModel.loadPhotos("0")
+            assertEquals(fakeUris, addPhotosViewModel.uiState.value.listUri)
+        } finally {
+            Dispatchers.resetMain()
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `test addUri correctly adds new uri to the list`() = runTest {
+        try {
+            // Set correctly the dispatcher
+            val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+            Dispatchers.setMain(testDispatcher)
+
+            // Set the model
+            val tripRepository = mock(TripsRepository::class.java)
+            val addPhotosViewModel = AddPhotosViewModel(tripRepository)
+
+            // Set up the mock to return the fake trip
+            `when`(tripRepository.getTrip("0")).thenReturn(fakeTrip)
+
+            // Load the state
+            addPhotosViewModel.loadPhotos("0")
+
+            // Add new photos to the state
+            val newUris = listOf("newUri1".toUri(), "newUri2".toUri())
+            addPhotosViewModel.addUri(newUris)
+
+            // Verify that the new list of Uris is the concatenation of the
+            val expectedList = fakeUris + newUris
+            assertEquals(expectedList, addPhotosViewModel.uiState.value.listUri)
+        } finally {
+            Dispatchers.resetMain()
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `test saveUri correctly adds new uri to the repository`() = runTest {
+        try {
+
+            // Set the dispatcher
+            val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+            Dispatchers.setMain(testDispatcher)
+
+            // Set the model
+            val fakeRepo = TripRepositoryLocal()
+            fakeRepo.addTrip(fakeTrip)
+            val addPhotosViewModel = AddPhotosViewModel(fakeRepo)
+
+            // Load the state
+            addPhotosViewModel.loadPhotos("1")
+
+            // Add new photos to the state
+            val newUris = listOf("newUri1".toUri(), "newUri2".toUri())
+            addPhotosViewModel.addUri(newUris)
+
+            // Save to the repository
+            addPhotosViewModel.savePhotos("1")
+            val expectedList = fakeUris + newUris
+
+            // Verify
+            assertEquals(expectedList, fakeRepo.getTrip("1").listUri)
+        } finally {
+            Dispatchers.resetMain()
+        }
+    }
+}
