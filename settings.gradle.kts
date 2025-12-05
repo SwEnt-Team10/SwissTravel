7f777957a939830f6bb3657c0f9e3f81b7952488
Lionel Hegetschweiler <lionel.heget@gmail.com>
2025-12-05 09:29:52 +0100

    refactor(deps): simplify Mapbox token configuration in settings
    Cleaned up the `settings.gradle.kts` file by removing a redundant `credentials` block. The logic for retrieving the `MAPBOX_DOWNLOADS_TOKEN` from either Gradle properties or environment variables is now handled in a single, more concise block.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,23 +18,19 @@ dependencyResolutionManagement {
         mavenCentral()
         maven {
             url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
-            credentials {
-                // used for private mapbox dependencies such as
-                // "com.mapbox.navigationcore:android-ndk27:3.16.0-beta.1"
-                username = "mapbox"
-                password = System.getenv("MAPBOX_DOWNLOADS_TOKEN")
-            }
             authentication {
                 create<BasicAuthentication>("basic")
             }
             credentials {
-                // Do not change the username below.
-                // This should always be `mapbox` (not your username).
+                // This should always be `mapbox`
                 username = "mapbox"
-                // Use the secret token you stored in gradle.properties (from user directory, not project) as the password
-                password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").orElse(providers.environmentVariable("MAPBOX_DOWNLOADS_TOKEN")).orNull
+                // Read from gradle.properties or env
+                password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN")
+                    .orElse(providers.environmentVariable("MAPBOX_DOWNLOADS_TOKEN"))
+                    .orNull
             }
         }
+
     }
 }
 
