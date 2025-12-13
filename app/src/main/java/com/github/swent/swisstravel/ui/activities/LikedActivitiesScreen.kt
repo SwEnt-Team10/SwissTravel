ebed213026e74a06ea59a2171e31b436a48480ac
Vincent du Fresne <vincent@du-fresne.ch>
2025-12-13 15:46:20 +0100

    style: use the "by" operator for convention and style to access the viewModel's state

--- app/src/main/java/com/github/swent/swisstravel/ui/activities/LikedActivitiesScreen.kt
+++ app/src/main/java/com/github/swent/swisstravel/ui/activities/LikedActivitiesScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -50,7 +51,7 @@ object LikedActivitiesScreenTestTags {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LikedActivitiesScreen(onBack: () -> Unit = {}, tripInfoVM: TripInfoViewModelContract) {
-  val state = tripInfoVM.uiState.collectAsState().value
+  val state by tripInfoVM.uiState.collectAsState()
   val likedActivities = state.likedActivities
   val selectedActivities = state.selectedLikedActivities
   val context = LocalContext.current
